d8cb095e7614b81cba485723319c1c0c1b4a3f78
erikvw <ew2789@gmail.com>
2019-11-22 19:01:00 -0600

    register post_migrate signal in ambition-edc

--- ambition_auth/codenames_by_group.py
+++ ambition_auth/codenames_by_group.py
@@ -13,10 +13,8 @@ from .codenames import (
     screening,
 )
 
-unblinding_requestors = []
-unblinding_reviewers = []
 codenames_by_group[AUDITOR] = auditor
 codenames_by_group[CLINIC] = clinic
 codenames_by_group[SCREENING] = screening
-codenames_by_group[UNBLINDING_REQUESTORS] = unblinding_requestors
-codenames_by_group[UNBLINDING_REVIEWERS] = unblinding_reviewers
+codenames_by_group[UNBLINDING_REQUESTORS] = []
+codenames_by_group[UNBLINDING_REVIEWERS] = []
